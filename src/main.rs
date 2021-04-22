80feb19bff4308ba09bb355f19e94a36ffbf3c8d
Noah Meyerhans <frodo@morgul.net>
2021-04-22 11:46:38 -0700

    print product info

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,7 @@ fn get_dmi_key(key: &str) -> Result<String, io::Error> {
     Ok(r)
 }
 
-fn print_vendor_data() {
-    let dmi_info_name_keys = [
-	("System", "sys_vendor"),
-	("BIOS", "bios_vendor"),
-	("Chassis", "chassis_vendor"),
-	("Board", "board_vendor"),
-    ];
-    println!("Vendor information:");
+fn print_dmi_id_fields(dmi_info_name_keys: &[(&str, &str)]) {
     for dmi_name_key in dmi_info_name_keys.iter() {
 	let sysfs_key = dmi_name_key.1;
 	let data = get_dmi_key(&sysfs_key);
@@ -27,6 +20,33 @@ fn print_vendor_data() {
 	println!("  - {} is {}", dmi_name_key.0, data);
     }
 }
+
+fn print_vendor_data() {
+    let dmi_info_name_keys = [
+	("System", "sys_vendor"),
+	("BIOS", "bios_vendor"),
+	("Chassis", "chassis_vendor"),
+	("Board", "board_vendor"),
+    ];
+    println!("Vendor information:");
+    print_dmi_id_fields(&dmi_info_name_keys);
+}
+
+fn print_product_data() {
+    let dmi_info_name_keys = [
+	("Family", "product_family"),
+	("Name", "product_name"),
+	("Serial", "product_serial"),
+	("SKU", "product_sku"),
+	("UUID", "product_uuid"),
+	("Version", "product_version"),
+    ];
+
+    println!("Product information:");
+    print_dmi_id_fields(&dmi_info_name_keys);
+}
+
 fn main() {
     print_vendor_data();
+    print_product_data();
 }
