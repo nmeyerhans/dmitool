bc3892366ccade6b38f4aa14b63dd87f4abdeeb3
Noah Meyerhans <frodo@morgul.net>
2021-04-23 11:13:56 -0700

    Print the whole decoded BIOS characteristics table

--- src/main.rs
+++ src/main.rs
@@ -111,12 +111,82 @@ fn decode_bios_extension_byte2(data: &Vec<u8>) {
     }
 }
 
-fn print_raw_table(table: &str, data: &Vec<u8>) {
+fn decode_byte(b: u8, bit_strings: &[(u8, &str)]) {
+    for bit in bit_strings.iter() {
+        if (b & bit.0) != 0 {
+            println!("  + {}", bit.1);
+        }
+    }
+}
+
+fn print_bios_table(table: &str, data: &Vec<u8>) {
     println!("Printing table {}", table);
-    if data.len() > 19 {
+    if data[0] != 0 {
+        println!("Invalid byte 0 in BIOS characteristics table!");
+        return;
+    }
+    let len: u8 = data[1];
+    if len < 0x12 {
+        println!("Invalid BIOS characteristics table length {}", len);
+        return;
+    }
+    let pos = 0xa;
+    if data[pos] & (1 << 3) != 0 {
+        // Does "BIOS Characteristics are not supported" really mean we should skip this table?
+        println!("BIOS Characteristics not supported on this system");
+        return;
+    }
+    let bit_strings = [
+        (1 << 4, "ISA is supported"),
+        (1 << 5, "MCA is supported"),
+        (1 << 6, "EISA is supported"),
+        (1 << 7, "PCI is supported"),
+    ];
+    decode_byte(data[pos], &bit_strings);
+    let pos = pos + 1;
+    let bit_strings = [
+        (1, "PCMCI is supported"),
+        (1 << 1, "PnP is supported"),
+        (1 << 2, "APM is supported"),
+        (1 << 3, "BIOS upgrades are supported"),
+        (1 << 4, "BIOS shadowing is allowed"),
+        (1 << 5, "VL-VESA is supported"),
+        (1 << 6, "ESCD support is available"),
+        (1 << 7, "Boot from CD is supported"),
+    ];
+    decode_byte(data[pos], &bit_strings);
+    let pos = pos + 1;
+    let bit_strings = [
+        (1, "Selectable boot is supported"),
+        (1 << 1, "BIOS ROM is socketed"),
+        (1 << 2, "Boot from PCMCIA (PC Card) is supported"),
+        (1 << 3, "EDD Specification is supported"),
+        (1 << 4, "Int 13h: NEC 9800 1.2 MB floppy is supported"),
+        (1 << 5, "Int 13h: Toshiba 1.2 MB Floppy is supported"),
+        (1 << 6, "Int 13h: 5.25” 360 KB floppy is supportd"),
+        (1 << 7, "Int 13h: 5.25” 1.2 MB floppy is supported"),
+    ];
+    decode_byte(data[pos], &bit_strings);
+    let pos = pos + 1;
+    let bit_strings = [
+        (1, "Int 13h: 3.5” / 720 KB floppy services are supported"),
+        (
+            1 << 1,
+            "Int 13h: 3.5” / 2.88 MB floppy services are supported",
+        ),
+        (1 << 2, "Int 5h: print screen Service is supported"),
+        (1 << 3, "Int 9h: 8042 keyboard services are supported"),
+        (1 << 4, "Int 14h: serial services are supported"),
+        (1 << 5, "Int 17h: printer services are supported"),
+        (1 << 6, "Int 10h: CGA/Mono Video Services are supported"),
+        (1 << 7, "NEC PC-98"),
+    ];
+    decode_byte(data[pos], &bit_strings);
+
+    if len > 19 {
         decode_bios_extension_byte1(data);
     }
-    if data.len() > 20 {
+    if len > 20 {
         decode_bios_extension_byte2(data);
     }
 }
@@ -139,7 +209,7 @@ fn main() {
         let table = "0-0";
         let res = read_table(&table);
         match res {
-            Ok(table_data) => print_raw_table(&table, &table_data),
+            Ok(table_data) => print_bios_table(&table, &table_data),
             Err(e) => println!("Reading table {}: {}", table, e),
         }
     } else {
