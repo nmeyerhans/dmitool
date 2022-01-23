0f865766f746d9a349931bafeef1bc5c05a802a4
Noah Meyerhans <frodo@morgul.net>
2022-01-22 21:07:12 -0800

    Decode more table 0 (BIOS) bytes
    Decode version 2.4-3.1 fields 0x13-0x18

--- src/dmi/table/table0.rs
+++ src/dmi/table/table0.rs
@@ -44,7 +44,7 @@ impl Table {
             (1 << 6, "1394 boot is supported"),
             (1 << 7, "Smart battery is supported"),
         ];
-        println!("BIOS Characteristics Extension byte 1:");
+        writeln!(f, "BIOS Characteristics Extension byte 1:")?;
         for bit in bit_strings.iter() {
             if (b & bit.0) != 0 {
                 writeln!(f, "  + {}", bit.1)?;
@@ -55,6 +55,7 @@ impl Table {
 
     fn decode_bios_extension_byte2(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let b: u8 = self.data.bits[19];
+        debug!("decoding byte2, value is {}", b);
         let bit_strings = [
             (1, "BIOS Boot Specification is supported"),
             (1 << 1, "F-Key initiated network boot is supported"),
@@ -63,7 +64,7 @@ impl Table {
             (1 << 4, "SMBIOS table describes a virtual machine"),
             /* Remaining bits are reserved for future use */
         ];
-        println!("BIOS Characteristics Extension byte 2:");
+        writeln!(f, "BIOS Characteristics Extension byte 2:")?;
         for bit in bit_strings.iter() {
             if (b & bit.0) != 0 {
                 writeln!(f, "  + {}", bit.1)?;
@@ -150,15 +151,45 @@ impl Table {
         ];
         decode_byte(f, self.data.bits[pos], &bit_strings)?;
 
-        if len > 19 {
+        if len > 0x13 {
             self.decode_bios_extension_byte1(f)?;
         }
-        if len > 20 {
+        if len > 0x14 {
             self.decode_bios_extension_byte2(f)?;
         }
         self.fmt_vendor(f)?;
         self.fmt_version(f)?;
         self.fmt_date(f)?;
+
+        if len > 0x18 {
+            // Spec version 2.4-3.0
+            if self.data.bits[0x14] != 0xff && self.data.bits[0x15] != 0xff {
+                writeln!(
+                    f,
+                    "BIOS Revision: {}.{}",
+                    self.data.bits[0x14], self.data.bits[0x15]
+                )?;
+            }
+
+            if self.data.bits[0x16] != 0xff && self.data.bits[0x17] != 0xff {
+                writeln!(
+                    f,
+                    "Firmware Revision: {}.{}",
+                    self.data.bits[0x16], self.data.bits[0x17]
+                )?;
+            }
+        }
+
+        if len > 0x19 {
+            let unit: &str = match self.data.bits[0x19] & 192 {
+                0 => "MB",
+                64 => "GB",
+                _ => "Unknown",
+            };
+            let sz: u16 =
+                u16::from(self.data.bits[0x19] & 63) << 8 | u16::from(self.data.bits[0x18]);
+            writeln!(f, "BIOS ROM Size: {} {}", sz, unit)?;
+        }
         Ok(())
     }
 }
@@ -219,5 +250,116 @@ mod tests {
         assert!(r.contains("Int 14h: serial services are supported"));
         assert!(r.contains("BIOS starts at memory location 0x800e"));
         assert!(r.contains("BIOS size is 192 kB"));
+    }
+
+    #[test]
+    fn test_decode_table0_v31() {
+        let d = Data {
+            location: 0,
+            string_location: 0,
+            next_loc: 0,
+            bits: [
+                0,    //type
+                0x20, // length is 0x20
+                0x0,  // handle (2 bytes)
+                0x1,
+                1,    // BIOS vendor name string
+                2,    // BIOS version string
+                0x0e, // BIOS starting address segment (2 bytes)
+                0x80,
+                3,      // BIOS release date string
+                2,      // BIOS size (n + 1 64 kB chunks)
+                128,    // BIOS characteristics (8 bytes)
+                128,    // CDROM boot is supported
+                1,      // selectable boot is supported
+                1 << 4, // serial support
+                0,
+                0,
+                0,
+                0,
+                3,  // extension byte 1
+                11, // extension byte 2
+                1,  // BIOS release major
+                1,  // BIOS release minor
+                5,  // Firmware release major
+                99, // Firmware release minor
+                1,  // BIOS extended size low bits
+                64, // BIOS extended size hight bits
+                0,
+            ]
+            .to_vec(),
+            strings: [
+                String::from("ACME Widgets, Inc."),
+                String::from("1.0a+3"),
+                String::from("5/8/1977"),
+            ]
+            .to_vec(),
+        };
+        let table = Table {
+            id: TableId::Bios,
+            data: d,
+        };
+        let r = format!("{}", table);
+        println!("{}", r);
+        assert!(r.contains("BIOS Characteristics"));
+        assert!(r.contains("Table handle is 256"));
+        assert!(r.contains("BIOS Vendor: ACME Widgets, Inc."));
+        assert!(r.contains("BIOS Version: 1.0a+3"));
+        assert!(r.contains("BIOS Release Date: 5/8/1977"));
+        assert!(r.contains("PCI is supported"));
+        assert!(r.contains("Boot from CD is supported"));
+        assert!(r.contains("Selectable boot is supported"));
+        assert!(r.contains("Int 14h: serial services are supported"));
+        assert!(r.contains("BIOS starts at memory location 0x800e"));
+        assert!(r.contains("BIOS size is 192 kB"));
+        assert!(r.contains("BIOS Revision: 1.1"));
+        assert!(r.contains("Firmware Revision: 5.99"));
+        assert!(r.contains("BIOS ROM Size: 1 GB"));
+        assert!(r.contains("ACPI is supported"));
+        assert!(r.contains("USB Legacy is supported"));
+        assert!(r.contains("BIOS Boot Specification is supported"));
+        assert!(r.contains("F-Key initiated network boot is supported"));
+        assert!(r.contains("UEFI Specification is supported"));
+    }
+
+    #[test]
+    fn test_decode_table0_with_empty_strings() {
+        let d = Data {
+            location: 0,
+            string_location: 0,
+            next_loc: 0,
+            bits: [
+                0,    //type
+                0x12, // length is 0x12 + entension bytes (zero here)
+                0x0,  // handle (2 bytes)
+                0x1,
+                1,    // BIOS vendor name string
+                2,    // BIOS version string
+                0x0e, // BIOS starting address segment (2 bytes)
+                0x80,
+                3,      // BIOS release date string
+                2,      // BIOS size (n + 1 64 kB chunks)
+                128,    // BIOS characteristics (8 bytes)
+                128,    // CDROM boot is supported
+                1,      // selectable boot is supported
+                1 << 4, // serial support
+                0,
+                0,
+                0,
+                0,
+            ]
+            .to_vec(),
+            strings: [].to_vec(),
+        };
+        let table = Table {
+            id: TableId::Bios,
+            data: d,
+        };
+        let r = format!("{}", table);
+        println!("{}", r);
+        assert!(r.contains("BIOS size is 192 kB"));
+        assert!(r.contains("BIOS Vendor: String index out of range. Buggy firmware?"));
+        assert!(r.contains("BIOS Version: String index out of range. Buggy firmware?"));
+        assert!(r.contains("BIOS Release Date: String index out of range. Buggy firmware?"));
     }
 }
