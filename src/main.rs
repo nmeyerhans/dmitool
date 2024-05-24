nmeyerhans/dmitool

commit 16b964280d5a0aab61375fb152ce1c13b47d2867

Update to clap 4

src/main.rs
@@ -29,7 +29,7 @@ extern crate log;
 
 use env_logger::Env;
 
-use clap::{App, Arg};
+use clap::{Command, Arg};
 
 const DMI_ID_ROOT: &str = "/sys/class/dmi/id";
 const DMI_ENTRIES_ROOT: &str = "/sys/firmware/dmi/entries";
@@ -128,18 +128,18 @@ fn do_table(id: u8) {
 }
 
 fn main() {
-    let args = App::new("DMI decoder tool")
+    let args = Command::new("DMI decoder tool")
         .version("0.1.0")
         .author("Noah Meyerhans <frodo@morgul.net>")
         .about("Decodes and prints system information from the SMBIOS")
         .arg(
-            Arg::with_name("zero")
+            Arg::new("zero")
                 .short('0')
                 .takes_value(false)
                 .help("print table 0 via the /sys/firmware/dmi/entries interface"),
         )
         .arg(
-            Arg::with_name("table")
+            Arg::new("table")
                 .short('t')
                 .long("table")
                 .takes_value(true)
@@ -148,7 +148,7 @@ fn main() {
                 .help("print the given table via the /sys/firmware/dmi/tables"),
         )
         .arg(
-            Arg::with_name("entrypoint")
+            Arg::new("entrypoint")
                 .short('e')
                 .long("entrypoint")
                 .takes_value(false)
@@ -156,7 +156,7 @@ fn main() {
                 .help("read SMBIOS entrypoint"),
         )
         .arg(
-	    Arg::with_name("debug")
+	    Arg::new("debug")
 		.short('d')
 		.long("debug")
 		.takes_value(false)
@@ -164,7 +164,7 @@ fn main() {
 	)
 	.get_matches();
 
-    if args.is_present("debug") {
+    if args.contains_id("debug") {
 	env::set_var("LOG_LEVEL", "debug")
     }
 
@@ -174,7 +174,7 @@ fn main() {
 
     env_logger::init_from_env(env);
 
-    if args.is_present("zero") {
+    if args.contains_id("zero") {
         info!("Getting table zero");
         let table = "0-0";
         let res = read_table(table);
@@ -182,10 +182,10 @@ fn main() {
             Ok(t) => print!("Table {}\n{}", &table, &t),
             Err(e) => panic!("Reading table {}: {}", table, e),
         }
-    } else if args.is_present("entrypoint") {
+    } else if args.contains_id("entrypoint") {
         do_entrypoint();
-    } else if args.is_present("table") {
-        let table_id: u8 = match args.value_of("table").unwrap().parse() {
+    } else if args.contains_id("table") {
+        let table_id: u8 = match args.get_one::<String>("table").unwrap().parse() {
             Ok(t) => t,
             Err(_e) => panic!("unable to parse table ID"),
         };

commit 58036ce7b9af41854a03b758a1a7ab171700e89c

Finish upgrading to clap 4.x
The earlier commit only reflected the changes driven by `cargo check
--features clap/deprecated`, not the actual update to the newer clap
version.  This change fixes that.

Fixes: 16b964280 ("Update to clap 4")

Cargo.toml
@@ -8,7 +8,7 @@ license = "GPL-2.0-only"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
-clap = "3.0"
+clap = "4.0"
 log = "0.4"
 env_logger = "0.8"
 trycmd = "0.15.3"

src/main.rs
@@ -135,14 +135,14 @@ fn main() {
         .arg(
             Arg::new("zero")
                 .short('0')
-                .takes_value(false)
+                .num_args(0)
                 .help("print table 0 via the /sys/firmware/dmi/entries interface"),
         )
         .arg(
             Arg::new("table")
                 .short('t')
                 .long("table")
-                .takes_value(true)
+                .num_args(1)
                 .value_name("TABLE")
                 .conflicts_with("zero")
                 .help("print the given table via the /sys/firmware/dmi/tables"),
@@ -151,7 +151,7 @@ fn main() {
             Arg::new("entrypoint")
                 .short('e')
                 .long("entrypoint")
-                .takes_value(false)
+                .num_args(0)
                 .conflicts_with("zero")
                 .help("read SMBIOS entrypoint"),
         )
@@ -159,12 +159,12 @@ fn main() {
 	    Arg::new("debug")
 		.short('d')
 		.long("debug")
-		.takes_value(false)
+		.num_args(0)
 		.help("enable debug output")
 	)
 	.get_matches();
 
-    if args.contains_id("debug") {
+    if args.get_flag("debug") {
 	env::set_var("LOG_LEVEL", "debug")
     }
 

tests/cmd/h.stdout
@@ -1,14 +1,11 @@
-DMI decoder tool 0.1.0
-Noah Meyerhans <frodo@morgul.net>
 Decodes and prints system information from the SMBIOS
 
-USAGE:
-    dmitool [OPTIONS]
+Usage: dmitool [OPTIONS]
 
-OPTIONS:
-    -0                     print table 0 via the /sys/firmware/dmi/entries interface
-    -d, --debug            enable debug output
-    -e, --entrypoint       read SMBIOS entrypoint
-    -h, --help             Print help information
-    -t, --table <TABLE>    print the given table via the /sys/firmware/dmi/tables
-    -V, --version          Print version information
+Options:
+  -0                   print table 0 via the /sys/firmware/dmi/entries interface
+  -t, --table <TABLE>  print the given table via the /sys/firmware/dmi/tables
+  -e, --entrypoint     read SMBIOS entrypoint
+  -d, --debug          enable debug output
+  -h, --help           Print help
+  -V, --version        Print version

tests/cmd/help.stdout
@@ -1,14 +1,11 @@
-DMI decoder tool 0.1.0
-Noah Meyerhans <frodo@morgul.net>
 Decodes and prints system information from the SMBIOS
 
-USAGE:
-    dmitool [OPTIONS]
+Usage: dmitool [OPTIONS]
 
-OPTIONS:
-    -0                     print table 0 via the /sys/firmware/dmi/entries interface
-    -d, --debug            enable debug output
-    -e, --entrypoint       read SMBIOS entrypoint
-    -h, --help             Print help information
-    -t, --table <TABLE>    print the given table via the /sys/firmware/dmi/tables
-    -V, --version          Print version information
+Options:
+  -0                   print table 0 via the /sys/firmware/dmi/entries interface
+  -t, --table <TABLE>  print the given table via the /sys/firmware/dmi/tables
+  -e, --entrypoint     read SMBIOS entrypoint
+  -d, --debug          enable debug output
+  -h, --help           Print help
+  -V, --version        Print version